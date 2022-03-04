2b968f82142708ab42d770a1a29db2a0e752a967
jonatan.ponzo <jonatan.ponzo@rappi.com>
2022-03-04 15:41:50 -0300

    exclude none

--- chat_api/api/v1/endpoints/messages.py
+++ chat_api/api/v1/endpoints/messages.py
@@ -12,14 +12,14 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.post("/", response_description="Create new message", dependencies=[Depends(JWTBearer())], response_model=MessageSchema)
+@router.post("/", response_description="Create new message", dependencies=[Depends(JWTBearer())], response_model=MessageSchema, response_model_exclude_none=True)
 def create_message(message: MessageCreateSchema = Body(...), db: Session = Depends(get_db_client)):
     created_message = MessageCrud.create(db, message)
     return created_message
 
 
 @router.get(
-    "/", response_description="Get messages", response_model=List[MessageSchema]
+    "/", response_description="Get messages", response_model=List[MessageSchema], response_model_exclude_none=True
 )
 def get_by_recipient(recipient_id: str, message_start_id: str, limit: int = 100, db: Session = Depends(get_db_client)):
     messages = MessageCrud.get_messages_by_recipient(db, recipient_id, message_start_id, limit)
@@ -27,7 +27,7 @@ def get_by_recipient(recipient_id: str, message_start_id: str, limit: int = 100,
 
 
 @router.get(
-    "/{id}", response_description="Get a single message", response_model=MessageSchema
+    "/{id}", response_description="Get a single message", response_model=MessageSchema, response_model_exclude_none=True
 )
 def get_message(id: str, db: Session = Depends(get_db_client)):
     message = MessageCrud.get(db, id)
